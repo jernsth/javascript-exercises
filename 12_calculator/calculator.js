8b51eecf79fe627dabc82459e3e3ba2ab442a080
jernsth <jonashermsen@gmx.de>
2025-09-25 10:32:36 +0200

    solved the calculator exercise

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = function(num1,num2) {
+	return num1+num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1,num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(sumArray) {
+	return sumArray.reduce(
+    (accumulator, currentValue) => accumulator + currentValue,
+    0
+  );
 };
 
-const multiply = function() {
-
+const multiply = function(multArray) {
+  	return multArray.reduce(
+    (accumulator, currentValue) => accumulator * currentValue,
+    1
+  );
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+	let result = base;
+  for(let i=1; i<exponent; i++) {
+    result *= base;
+  }
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) { 
+  let result = 1;
+  for( let i=num; i>0; i--) {
+    result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line
