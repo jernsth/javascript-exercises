c1acd5ce050d53f2c163a9e4f377ab1b528396ca
jernsth <jonashermsen@gmx.de>
2025-09-26 12:24:44 +0200

    solved ex16

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    let current=2025;
+    let mappedToAge = array.map(item => ({
+        name: item.name,
+        age: item.yearOfDeath ? (item.yearOfDeath - item.yearOfBirth) : (current - item.yearOfBirth)
+    }));
+    let maxItem = mappedToAge.reduce( (max, item) => {
+        return item.age > max.age ? item : max;
+    });
+    return maxItem;
 };
 
 // Do not edit below this line
