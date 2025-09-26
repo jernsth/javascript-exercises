a8a293293af534c533d051891f3cb694cb7218e9
jernsth <jonashermsen@gmx.de>
2025-09-26 11:55:44 +0200

    solved palindromes and fibonacci

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,29 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    // remove whitespaces and dots.
+    let strArr = string.toLowerCase().split("");
+    strArr = strArr.filter((item) => item.match(/[a-z0-9]/));
+    console.log(strArr);
 
+    // Take two pointers starting at the left and the right of the array
+    let pointer1 = 0;
+    let pointer2 = strArr.length-1;
+
+    // Let them run through array and check if they are the same
+    result = true;
+    for(let i = 0; i<Math.ceil(((strArr.length-1) / 2)); i++) {
+        console.log(strArr[pointer1] + strArr[pointer2])
+        if(strArr[pointer1] == strArr[pointer2]) {
+            result = true;
+            pointer1++;
+            pointer2--;
+        }
+        else {
+            return false;
+        }
+    }
+    return result;
 };
 
+console.log(palindromes("A car, a man, a maraca."));
 // Do not edit below this line
 module.exports = palindromes;
--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    if (!Number.isInteger(Number(num)) || Number(num) < 0) {
+        return "OOPS";
+    } else if (Number(num) <= 2 && Number(num) > 0) {
+        return 1;
+    }
+    
+    let fib1 = 1;
+    let fib2 = 1;
+    let temp = 0;
 
+    for (let i=2; i<Number(num); i++) {
+        temp = fib1 + fib2;
+        fib1 = fib2;
+        fib2 = temp;
+    }
+
+    return temp;
 };
 
 // Do not edit below this line
