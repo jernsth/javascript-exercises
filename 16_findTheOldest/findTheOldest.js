6d659c650745af5477a3932aa74ecc102403bffd
jernsth <jonashermsen@gmx.de>
2025-09-26 12:25:34 +0200

    prettified code

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,9 +1,14 @@
 const findTheOldest = function(array) {
+    // current year
     let current=2025;
+
+    // map the input array to age instead of years
     let mappedToAge = array.map(item => ({
         name: item.name,
         age: item.yearOfDeath ? (item.yearOfDeath - item.yearOfBirth) : (current - item.yearOfBirth)
     }));
+
+    // find the maximum Age
     let maxItem = mappedToAge.reduce( (max, item) => {
         return item.age > max.age ? item : max;
     });
